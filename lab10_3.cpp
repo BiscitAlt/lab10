54e55c779b8e9bf39925d2ad5b21198cf7d01c7b
BiscitAlt <thitiputt.pe@gmail.com>
2025-12-26 23:59:10 +0700

    lab10

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main (){
+	ifstream source;
+	source.open("score.txt");
+	string textline;
+    int count = 0;
+    float mean,sd,sum;
+	while(getline(source,textline)){
+		sum += atof(textline.c_str());
+        count++;
+	}
+    mean = sum/(float)count;
+    while(getline(source,textline)){
+        sd += pow(abs(atof(textline.c_str())-mean),2);
+	}
+    sd = sqrt(sd/count);
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << sd << '\n';
+    source.close();
+	return 0;
+}
